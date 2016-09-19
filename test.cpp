9aa932ab08084d43205e37e3d7a62a6b82df645a
HamishONE <hone075@aucklanduni.ac.nz>
2016-09-19 20:31:19 +1200

    Add tests from piazza @418

--- test.cpp
+++ test.cpp
@@ -204,6 +204,41 @@ TestResult test_SimpleLaneEnqueue() {
     return TR_PASS;
 }
 
+// from piazza @418
+TestResult test_SimpleLaneDequeue() {
+	SimpleLane lane;
+	Vehicle* v1 = new Vehicle(Vehicle::VT_CAR, 1);
+	Vehicle* v2 = new Vehicle(Vehicle::VT_CAR, 2);
+	Vehicle* v3 = new Vehicle(Vehicle::VT_BUS, 3);
+	Vehicle* m1 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+	lane.enqueue(v1);
+	lane.enqueue(v2);
+	lane.enqueue(v3);
+	lane.enqueue(m1);
+	ASSERT(lane.dequeue() == v1);
+	ASSERT(lane.count() == 3)
+	ASSERT(lane.front() == v2);
+	ASSERT(lane.back() == m1);
+	ASSERT(lane.dequeue() == v2);
+	ASSERT(lane.count() == 2)
+	ASSERT(lane.front() == v3);
+	ASSERT(lane.back() == m1);
+	ASSERT(lane.dequeue() == v3);
+	ASSERT(lane.count() == 1)
+	ASSERT(lane.front() == m1);
+	ASSERT(lane.back() == m1);
+	ASSERT(lane.dequeue() == m1);
+	ASSERT(lane.count() == 0)
+	ASSERT(lane.front() == 0);
+	ASSERT(lane.back() == 0);
+	ASSERT(lane.dequeue() == 0);
+	ASSERT(lane.count() == 0)
+	ASSERT(lane.front() == 0);
+	ASSERT(lane.back() == 0);
+
+    return TR_PASS;
+}
+
 /*
 Make sure ExpressLane exhibits the same initial state as SimpleLane
 */
@@ -220,9 +255,7 @@ TestResult test_ExpressLaneInitialState() {
     return TR_PASS;
 }
 
-/*
-Test adding vehicles to the SimpleLane.
-*/
+// made by hamish
 TestResult test_ExpressLaneEnqueue() {
     ExpressLane lane;
     Vehicle* v1 = new Vehicle(Vehicle::VT_CAR, 1);
@@ -283,6 +316,100 @@ TestResult test_ExpressLaneEnqueue() {
 	ASSERT(lane.back() == 0);
 	ASSERT(lane.front() == 0);
 	ASSERT(lane.dequeue() == 0);
+
+    // vehicles should be deleted by the lane
+
+    return TR_PASS;
+}
+
+// from piazza @418
+TestResult test_ExpressLaneEnqueue2() {
+    ExpressLane lane;
+    Vehicle* v1 = new Vehicle(Vehicle::VT_CAR, 1);
+    Vehicle* v2 = new Vehicle(Vehicle::VT_CAR, 2);
+    Vehicle* v3 = new Vehicle(Vehicle::VT_BUS, 3);
+    Vehicle* m1 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+
+    lane.enqueue(v1);
+    ASSERT(!lane.empty())
+    ASSERT(lane.count() == 1)
+    ASSERT(lane.back() == v1);
+    ASSERT(lane.front() == v1);
+    lane.enqueue(v2);
+    ASSERT(!lane.empty())
+    ASSERT(lane.count() == 2)
+    ASSERT(lane.back() == v2);
+    ASSERT(lane.front() == v1);
+    lane.enqueue(v3);
+    ASSERT(!lane.empty())
+    ASSERT(lane.count() == 3)
+    ASSERT(lane.back() == v3);
+    ASSERT(lane.front() == v1);
+    lane.enqueue(m1);
+    ASSERT(!lane.empty())
+    ASSERT(lane.count() == 4)
+    ASSERT(lane.back() == v3);
+    ASSERT(lane.front() == m1);
+
+    ExpressLane lane2;
+    Vehicle* v4 = new Vehicle(Vehicle::VT_CAR, 1);
+    Vehicle* m2 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+
+    lane2.enqueue(v4);
+    ASSERT(!lane2.empty())
+    ASSERT(lane2.count() == 1)
+    ASSERT(lane2.back() == v4);
+    ASSERT(lane2.front() == v4);
+    lane2.enqueue(m2);
+    ASSERT(!lane2.empty())
+    ASSERT(lane2.count() == 2)
+    ASSERT(lane2.back() == v4);
+    ASSERT(lane2.front() == m2);
+
+    ExpressLane lane3;
+    Vehicle* m3 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+    Vehicle* m4 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+
+    lane3.enqueue(m3);
+    ASSERT(!lane3.empty())
+    ASSERT(lane3.count() == 1)
+    ASSERT(lane3.back() == m3);
+    ASSERT(lane3.front() == m3);
+    lane3.enqueue(m4);
+    ASSERT(!lane3.empty())
+    ASSERT(lane3.count() == 2);
+    ASSERT(lane3.back() == m4);
+    ASSERT(lane3.front() == m3);
+
+    ExpressLane lane4;
+    Vehicle* v5 = new Vehicle(Vehicle::VT_CAR, 1);
+    Vehicle* v6 = new Vehicle(Vehicle::VT_CAR, 2);
+    Vehicle* m5 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+    Vehicle* m6 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+
+    lane4.enqueue(v5);
+    ASSERT(!lane4.empty())
+    ASSERT(lane4.count() == 1)
+    ASSERT(lane4.back() == v5);
+    ASSERT(lane4.front() == v5);
+    lane4.enqueue(m5);
+    ASSERT(!lane4.empty())
+    ASSERT(lane4.count() == 2)
+    ASSERT(lane4.back() == v5);
+    ASSERT(lane4.front() == m5);
+    lane4.enqueue(v6);
+    ASSERT(!lane4.empty())
+    ASSERT(lane4.count() == 3)
+    ASSERT(lane4.back() == v6);
+    ASSERT(lane4.front() == m5);
+    lane4.enqueue(m6);
+    ASSERT(!lane4.empty())
+    ASSERT(lane4.count() == 4)
+    ASSERT(lane4.back() == v6);
+    ASSERT(lane4.front() == m5);
+
+    ASSERT(lane4.dequeue() == m5);
+    ASSERT(lane4.dequeue() == m6);
 
     // vehicles should be deleted by the lane
 
@@ -531,6 +658,200 @@ TestResult test_IntersectionTwoInOpposing() {
     return TR_PASS;
 }
 
+// from piazza @418
+TestResult test_IntersectionTwoInAdjacent() {
+    Intersection intersection;
+
+    // vehicles for testing
+    Vehicle* v1 = new Vehicle(Vehicle::VT_CAR, 1);
+    v1->turnStraight();
+    Vehicle* v2 = new Vehicle(Vehicle::VT_BUS, 10);
+    v2->turnLeft();
+    Vehicle* v3 = new Vehicle(Vehicle::VT_CAR, 1);
+    v3->turnStraight();
+    Vehicle* v4 = new Vehicle(Vehicle::VT_MOTORCYCLE, 2);
+    v4->turnLeft();
+    Vehicle* v5 = new Vehicle(Vehicle::VT_CAR, 3);
+    v5->turnStraight();
+    Vehicle* v6 = new Vehicle(Vehicle::VT_CAR, 2);
+    v6->turnStraight();
+    Vehicle* v7 = new Vehicle(Vehicle::VT_BUS, 8);
+    v7->turnRight();
+    Vehicle* v8 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+    v8->turnStraight();
+
+    // create the Lanes
+    Lane* roadInSouth = new SimpleLane();
+    roadInSouth->enqueue(v1);
+    roadInSouth->enqueue(v2);
+    roadInSouth->enqueue(v3);
+    roadInSouth->enqueue(v4);
+
+    Lane* roadInEast = new SimpleLane();
+    roadInEast->enqueue(v5);
+    roadInEast->enqueue(v6);
+    roadInEast->enqueue(v7);
+    roadInEast->enqueue(v8);
+
+    Lane* roadOutNorth = new SimpleLane();
+    Lane* roadOutWest = new SimpleLane();
+
+    // add Lanes to the Intersection
+    ASSERT(intersection.connectNorth(roadOutNorth, Intersection::LD_OUTGOING) == 0); // exit
+    ASSERT(intersection.connectEast(roadInEast, Intersection::LD_INCOMING) == 0); // entry
+    ASSERT(intersection.connectSouth(roadInSouth, Intersection::LD_INCOMING) == 0); // entry
+    ASSERT(intersection.connectWest(roadOutWest, Intersection::LD_OUTGOING) == 0); // exit
+    // check intersection is valid
+    ASSERT(intersection.valid());
+
+    // simulate a single tick
+    intersection.simulate();
+    // check results (exactly one vehicle should proceed)
+    ASSERT(roadInSouth->front() == v2);
+    ASSERT(roadInEast->front() == v6);
+    ASSERT(roadOutNorth->front() == v1);
+    ASSERT(roadOutWest->front() == v5);
+
+
+    // run the simulation for 7 more ticks to allow the other vehicles to proceed (should be enough if road rules are
+    // followed)
+    for (int i = 0; i < 8; ++i) {
+        intersection.simulate();
+    }
+
+    // check all vehicles passed through the intersection successfully
+    // check inbound lanes
+    ASSERT(roadInEast->front() == 0);
+    ASSERT(roadInSouth->front() == 0);
+    // check northbound out lane
+    ASSERT(roadOutNorth->dequeue() == v1);
+    ASSERT(roadOutNorth->dequeue() == v3);
+    ASSERT(roadOutNorth->dequeue() == v7);
+    ASSERT(roadOutNorth->dequeue() == 0);
+    // check westbound out lane
+    ASSERT(roadOutWest->dequeue() == v5);
+    ASSERT(roadOutWest->dequeue() == v6);
+    ASSERT(roadOutWest->dequeue() == v2);
+    ASSERT(roadOutWest->dequeue() == v4);
+    ASSERT(roadOutWest->dequeue() == v8);
+    ASSERT(roadOutWest->dequeue() == 0);
+
+    delete roadInEast;
+    delete roadOutNorth;
+    delete roadInSouth;
+    delete roadOutWest;
+
+    delete v1;
+    delete v2;
+    delete v3;
+    delete v4;
+    delete v5;
+    delete v6;
+    delete v7;
+    delete v8;
+
+    return TR_PASS;
+}
+
+// from piazza @418
+TestResult test_IntersectionThreeIn() {
+    Intersection intersection;
+
+    // vehicles for testing
+    Vehicle* v1 = new Vehicle(Vehicle::VT_CAR, 1);
+    v1->turnLeft();
+    Vehicle* v2 = new Vehicle(Vehicle::VT_BUS, 10);
+    v2->turnLeft();
+    Vehicle* v3 = new Vehicle(Vehicle::VT_CAR, 1);
+    v3->turnLeft();
+    Vehicle* v4 = new Vehicle(Vehicle::VT_MOTORCYCLE, 2);
+    v4->turnStraight();
+    Vehicle* v5 = new Vehicle(Vehicle::VT_CAR, 3);
+    v5->turnStraight();
+    Vehicle* v6 = new Vehicle(Vehicle::VT_CAR, 2);
+    v6->turnStraight();
+    Vehicle* v7 = new Vehicle(Vehicle::VT_BUS, 8);
+    v7->turnRight();
+    Vehicle* v8 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+    v8->turnRight();
+    Vehicle* v9 = new Vehicle(Vehicle::VT_MOTORCYCLE, 1);
+    v9->turnRight();
+
+    // create the Lanes
+    Lane* roadInSouth = new SimpleLane();
+    roadInSouth->enqueue(v1);
+    roadInSouth->enqueue(v2);
+    roadInSouth->enqueue(v3);
+
+    Lane* roadInEast = new SimpleLane();
+    roadInEast->enqueue(v4);
+    roadInEast->enqueue(v5);
+    roadInEast->enqueue(v6);
+
+    Lane* roadInNorth = new SimpleLane();
+    roadInNorth->enqueue(v7);
+    roadInNorth->enqueue(v8);
+    roadInNorth->enqueue(v9);
+
+    Lane* roadOutWest = new SimpleLane();
+
+    // add Lanes to the Intersection
+    ASSERT(intersection.connectNorth(roadInNorth, Intersection::LD_INCOMING) == 0); // entry
+    ASSERT(intersection.connectEast(roadInEast, Intersection::LD_INCOMING) == 0); // entry
+    ASSERT(intersection.connectSouth(roadInSouth, Intersection::LD_INCOMING) == 0); // entry
+    ASSERT(intersection.connectWest(roadOutWest, Intersection::LD_OUTGOING) == 0); // exit
+    // check intersection is valid
+    ASSERT(intersection.valid());
+
+    // simulate a single tick
+    intersection.simulate();
+    // check results (exactly one vehicle should proceed)
+    ASSERT(roadInSouth->front() == v1);
+    ASSERT(roadInEast->front() == v5);
+    ASSERT(roadInNorth->front() == v7);
+    ASSERT(roadOutWest->front() == v4);
+
+    // run the simulation for 9 more ticks to allow the other vehicles to proceed (should be enough if road rules are
+    // followed)
+    for (int i = 0; i < 9; ++i) {
+        intersection.simulate();
+    }
+
+    // check all vehicles passed through the intersection successfully
+    // check inbound lanes
+    ASSERT(roadInEast->front() == 0);
+    ASSERT(roadInSouth->front() == 0);
+    ASSERT(roadInNorth->front() == 0);
+    // check westbound out lane
+    ASSERT(roadOutWest->dequeue() == v4);
+    ASSERT(roadOutWest->dequeue() == v5);
+    ASSERT(roadOutWest->dequeue() == v6);
+    ASSERT(roadOutWest->dequeue() == v1);
+    ASSERT(roadOutWest->dequeue() == v2);
+    ASSERT(roadOutWest->dequeue() == v3);
+    ASSERT(roadOutWest->dequeue() == v7);
+    ASSERT(roadOutWest->dequeue() == v8);
+    ASSERT(roadOutWest->dequeue() == v9);
+    ASSERT(roadOutWest->dequeue() == 0);
+
+    delete roadInEast;
+    delete roadInNorth;
+    delete roadInSouth;
+    delete roadOutWest;
+
+    delete v1;
+    delete v2;
+    delete v3;
+    delete v4;
+    delete v5;
+    delete v6;
+    delete v7;
+    delete v8;
+
+    return TR_PASS;
+}
+
+// made by hamish
 TestResult test_TrafficNetwork() {
 
 	Intersection i1, i2, i3, i4;
@@ -585,6 +906,113 @@ TestResult test_TrafficNetwork() {
 	for (int i=0; i<12; i++) delete lanes[i];
 
 	return TR_PASS;
+}
+
+// from piazza @418
+TestResult test_TrafficNetwork2() {
+    Intersection i0;
+    Intersection i1;
+    Intersection i2;
+    Intersection i3;
+
+    Vehicle* v1 = new Vehicle(Vehicle::VT_CAR, 1);
+    v1->turnLeft();
+    v1->turnStraight();
+    Vehicle* v2 = new Vehicle(Vehicle::VT_BUS, 10);
+    v2->turnStraight();
+    v2->turnRight();
+    v2->turnLeft();
+    Vehicle* v3 = new Vehicle(Vehicle::VT_CAR, 1);
+    v3->turnStraight();
+    v3->turnRight();
+    v3->turnLeft();
+    Vehicle* v4 = new Vehicle(Vehicle::VT_MOTORCYCLE, 2);
+    v4->turnLeft();
+    v4->turnRight();
+    v4->turnRight();
+    v4->turnRight();
+    v4->turnRight();
+    v4->turnRight();
+    v4->turnRight();
+    v4->turnStraight();
+
+    Lane* lanes[12];
+    for (int i=0;i<12;i++) {
+    	lanes[i] = new SimpleLane();
+    }
+
+    lanes[0]->enqueue(v1);
+    lanes[1]->enqueue(v2);
+    lanes[2]->enqueue(v3);
+    lanes[7]->enqueue(v4);
+
+    i0.connectNorth(lanes[0], Intersection::LD_INCOMING);
+    i0.connectEast(lanes[3], Intersection::LD_OUTGOING);
+    i0.connectSouth(lanes[5], Intersection::LD_INCOMING);
+    i0.connectWest(lanes[2], Intersection::LD_INCOMING);
+    i1.connectNorth(lanes[1], Intersection::LD_INCOMING);
+    i1.connectEast(lanes[4], Intersection::LD_OUTGOING);
+    i1.connectSouth(lanes[6], Intersection::LD_OUTGOING);
+    i1.connectWest(lanes[3], Intersection::LD_INCOMING);
+    i2.connectNorth(lanes[5], Intersection::LD_OUTGOING);
+    i2.connectEast(lanes[8], Intersection::LD_INCOMING);
+    i2.connectSouth(lanes[10], Intersection::LD_OUTGOING);
+    i2.connectWest(lanes[7], Intersection::LD_INCOMING);
+    i3.connectNorth(lanes[6], Intersection::LD_INCOMING);
+    i3.connectEast(lanes[9], Intersection::LD_OUTGOING);
+    i3.connectSouth(lanes[11], Intersection::LD_OUTGOING);
+    i3.connectWest(lanes[8], Intersection::LD_OUTGOING);
+
+    ASSERT(i0.valid());
+    ASSERT(i1.valid());
+    ASSERT(i2.valid());
+    ASSERT(i3.valid());
+
+    i0.simulate();
+    i1.simulate();
+    i3.simulate();
+    i2.simulate();
+    ASSERT(lanes[0]->front() == v1);
+    ASSERT(lanes[10]->front() == v2);
+    ASSERT(lanes[3]->front() == v3);
+    ASSERT(lanes[5]->front() == v4);
+
+    i0.simulate();
+    i1.simulate();
+    i3.simulate();
+    i2.simulate();
+    ASSERT(lanes[3]->front() == v1);
+    ASSERT(lanes[10]->front() == v2);
+    ASSERT(lanes[9]->front() == v3);
+    ASSERT(lanes[5]->front() == v4);
+
+    i0.simulate();
+    i1.simulate();
+    i3.simulate();
+    i2.simulate();
+    ASSERT(lanes[4]->front() == v1);
+    ASSERT(lanes[10]->front() == v2);
+    ASSERT(lanes[9]->front() == v3);
+    ASSERT(lanes[3]->front() == v4)
+
+    i0.simulate();
+    i1.simulate();
+    i3.simulate();
+    i2.simulate();
+    i0.simulate();
+    i1.simulate();
+    i3.simulate();
+    i2.simulate();
+    ASSERT(lanes[4]->front() == v1);
+    ASSERT(lanes[10]->front() == v2);
+    ASSERT(lanes[9]->front() == v3);
+    ASSERT(lanes[11]->front() == v4)
+
+    for (int i=0;i<12;i++) {
+    	delete lanes[i];
+    }
+
+    return TR_PASS;
 }
 
 // from piazza @301
@@ -766,15 +1194,20 @@ vector<TestResult (*)()> generateTests() {
     tests.push_back(&test_SimpleLaneConstruction);
     tests.push_back(&test_SimpleLaneInitialState);
     tests.push_back(&test_SimpleLaneEnqueue);
+    tests.push_back(&test_SimpleLaneDequeue);
     tests.push_back(&test_ExpressLaneInitialState);
     tests.push_back(&test_ExpressLaneEnqueue);
+    tests.push_back(&test_ExpressLaneEnqueue2);
 #endif /*ENABLE_T1_TESTS*/
 #ifdef ENABLE_T2_TESTS
     tests.push_back(&test_IntersectionConstruction);
     tests.push_back(&test_IntersectionOverwrite);
     tests.push_back(&test_IntersectionOneIn);
     tests.push_back(&test_IntersectionTwoInOpposing);
+    tests.push_back(&test_IntersectionTwoInAdjacent);
+    tests.push_back(&test_IntersectionThreeIn);
     tests.push_back(&test_TrafficNetwork);
+    tests.push_back(&test_TrafficNetwork2);
     tests.push_back(&test_Intersections);
     tests.push_back(&test_The_Filip_Simulate);
 #endif /*ENABLE_T2_TESTS*/
